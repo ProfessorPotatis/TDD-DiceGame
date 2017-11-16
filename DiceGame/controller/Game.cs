b9c629affffe9b198433cfbfb8dc5d7763f04a19
Sofia Kristiansen <sofiakristiansen@Sofias-MBP.lan>
2017-11-16 10:06:41 +0100

    GREEN

--- DiceGame/controller/Game.cs
+++ DiceGame/controller/Game.cs
@@ -4,15 +4,25 @@ namespace DiceGame.Controller
 {
     public class Game
     {
+        private View.ConsoleView _view;
 
         public Game(View.ConsoleView view)
         {
+            View = view;
+        }
 
+        public View.ConsoleView View
+        {
+            get { return _view; }
+            set
+            {
+                _view = value;
+            }
         }
 
         public void run()
         {
-            
+            View.showMenu();
         }
         
     }
