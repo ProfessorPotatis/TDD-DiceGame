5b70f8d5e1fb71420cb7f9227f2c897eb5a85d62
Sofia Kristiansen <sofiakristiansen@Sofias-MBP.lan>
2017-11-23 10:44:14 +0100

    GREEN

--- DiceGame/model/Dice.cs
+++ DiceGame/model/Dice.cs
@@ -5,9 +5,25 @@ namespace DiceGame.Model
 {
     public class Dice : IDice
     {
+        private Random _die;
+
+        public Dice()
+        {
+            Die = new Random();
+        }
+
+        public Random Die
+        {
+            get { return _die; }
+            set
+            {
+                _die = value;
+            }
+        }
+
         public int rollDice()
         {
-            return 0;
+            return Die.Next(1, 7);;
         }
 
     }
